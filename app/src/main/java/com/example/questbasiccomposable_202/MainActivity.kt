dd9b91f0e8bd93fd9ef4910747285cdd5460bc54
wawanpi <muhaguskurniawan@gmail.com>
2024-10-18 20:41:43 +0700

    Membuat compenen column

--- app/src/main/java/com/example/questbasiccomposable_202/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_202/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_202.ui.theme.QuestBasicComposable_202Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +32,7 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun BasicComposable(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
 
 }
 
@@ -44,5 +49,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     QuestBasicComposable_202Theme {
       //  Greeting("Android")
+    BasicComposable()
     }
 }
