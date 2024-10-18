80f423ee9222cc0c41ca30cc9f483fa908a87a0d
wawanpi <muhaguskurniawan@gmail.com>
2024-10-18 20:55:46 +0700

    Membuat text Ini Adalah Halaman Login

--- app/src/main/java/com/example/questbasiccomposable_202/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_202/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,10 +40,12 @@ fun BasicComposable(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(20.dp)
     ) {
-        Text("Login",  fontSize = 70.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.padding(10.dp))
+        Text("Login",  fontSize = 50.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(5.dp))
+        Text("Ini Adalah Halaman Login", fontSize = 20.sp, fontStyle = FontStyle.Italic)
+
     }
 
 }
